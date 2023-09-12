c6eedf643e261e3261fca00736e1be0e88cd9121
shun <108392107+shunkatagiri@users.noreply.github.com>
2023-09-12 02:20:54 +0000

    button(削除)タグを生成

--- src/index.mjs
+++ src/index.mjs
@@ -16,8 +16,12 @@ const onClickAdd =  () => {
   // button（完了)タグを生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
-  console.log(completeButton);
 
+
+  // button(削除)タグを生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  console.log(deleteButton)
   // divタグの子要素に各要素を設定
   div.appendChild(li);
   
