2fba7e8962de487620fe6ff6f2c16f0b342ccb1f
shun <108392107+shunkatagiri@users.noreply.github.com>
2023-09-12 03:59:20 +0000

    未完了リストから指定の要素を削除する挙動を関数にまとめる

--- src/index.mjs
+++ src/index.mjs
@@ -18,8 +18,7 @@ const onClickAdd =  () => {
   completeButton.innerText = "完了";
   completeButton.addEventListener("click" , () => {
     //押された完了ボタンの親タグ(div)を未完了ボタンから削除
-    const deleteTarget = completeButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteFromIncompleteList(completeButton.parentNode);
   });
 
 
@@ -28,8 +27,7 @@ const onClickAdd =  () => {
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click" ,() => {
      //　押された削除ボタンの親タグ(div)を未完了リストから削除
-     const deleteTarget = deleteButton.parentNode;
-     document.getElementById("incomplete-list").removeChild(deleteTarget);
+     deleteFromIncompleteList(deleteButton.parentNode);
     });
 
 
@@ -43,6 +41,11 @@ const onClickAdd =  () => {
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(div);
 };
+
+ // 未完了リストから指定の要素を削除
+ const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target); 
+ }
 
 document
   .getElementById("add-button")
