d274e96a52d54bf046335366b125ae2b4ed19428
shun <108392107+shunkatagiri@users.noreply.github.com>
2023-09-12 02:30:41 +0000

    完了と削除ボタンにイベントを付与する

--- src/index.mjs
+++ src/index.mjs
@@ -16,15 +16,25 @@ const onClickAdd =  () => {
   // button（完了)タグを生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+  completeButton.addEventListener("click" , () => {
+    alert("完了")
+  });
 
 
   // button(削除)タグを生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
-  console.log(deleteButton)
+  deleteButton.addEventListener("click" ,() => {
+    alert("削除");
+  });
+
+
+
   // divタグの子要素に各要素を設定
   div.appendChild(li);
-  
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(div);
 };
